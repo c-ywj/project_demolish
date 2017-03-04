8b150b32b56cb03ade4cbf82bf405a662c70171c
Florence <smc.florence@gmail.com>
2017-03-04 20:10:53 +0000

    working on appending items to browser

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,63 +1,36 @@
 $(() => {
-
+  let $orderCart = $('.order-items');
+  let $cartItems = $('.cart-items');
+  $orderCart.hide();
   
+  let currentOrder = {};
 
+  $('.menu-item').on('click', function (ev) {
+    $orderCart.show();
+
+    var itemId = $(ev.currentTarget).attr("data-item-id");
+    var itemName = $(ev.currentTarget).attr("data-item-name");
+    if(!currentOrder[itemName]) {
+      currentOrder[itemName] = {itemId: itemId, qty: 1};
+
+      let cartItem = `<li>
+        <h3>${itemName}</h3>
+        <div class="remove-item">
+          <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <div class="cart-qty">
+          Qty: 
+            <span class="cart-item__qty">${currentOrder[itemName]['qty']}</span>
+        </div>
+      </li>`;
+      let $cartItemQty = $('.cart-item__qty');
+      $cartItems.append(cartItem);
+
+    } else {
+      currentOrder[itemName]['qty'] += 1;
+      // $cartItemQty.remove();
+      // $cartItemQty.appendTo($('.cart-qty'));
+    }
+    console.log(currentOrder);
+  });
 });
-
-
-//   const getMenu = () => {
-//     $.ajax({
-//       method: 'GET',
-//       url: '/menu'
-//     })
-//     .then((menu_items) => {
-//       loadMenuItems(menu_items);
-//     })
-//     .catch(() => {
-//       console.error('There was an error in loading the menu...');
-//     })
-//   };
-
-//   // load menu on document ready
-//   getMenu();
-
-//   $('#ORDER-BUTTON').on('click', (e) => {
-//     e.preventDefault();
-
-//     $.ajax({
-//       method: 'POST',
-//       url: '/order',
-//       data: $orderItems.serialize()
-//     })
-//     .then((order_item) => {
-//       // what we want to do with the order stuff
-//     })
-//     .catch(() => {
-//       console.error('There was a problem processing the order...');
-//     });
-//   });
-
-
-
-
-
-//   // $.ajax({
-//   //   method: "GET",
-//   //   url: "/api/users"
-//   // }).done((users) => {
-//   //   for(user of users) {
-//   //     $("<div>").text(user.name).appendTo($("body"));
-//   //   }
-//   // });;
-
-// });
-
-//   $.ajax({
-//     method: "GET",
-//     url: "/api/users"
-//   }).done((users) => {
-//     for(user of users) {
-//       $("<div>").text(user.name).appendTo($("body"));
-//     }
-//   });
-// });
