5d31884a65cd42fd57d9767ff41fc762b26c733f
Florence <smc.florence@gmail.com>
2017-03-04 22:22:08 +0000

    working on removing items from browser and object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,32 +5,52 @@ $(() => {
   
   let currentOrder = {};
 
+  // add new item to cart
   $('.menu-item').on('click', function (ev) {
     $orderCart.show();
 
     var itemId = $(ev.currentTarget).attr("data-item-id");
     var itemName = $(ev.currentTarget).attr("data-item-name");
+
     if(!currentOrder[itemName]) {
       currentOrder[itemName] = {itemId: itemId, qty: 1};
 
-      let cartItem = `<li>
+      let cartItem = `<li data-cart-item="${itemName}">
         <h3>${itemName}</h3>
         <div class="remove-item">
           <i class="fa fa-times" aria-hidden="true"></i>
         </div>
-        <div class="cart-qty">
+        <div class="cart-qty ${itemName}">
+          <span class="reduce-item"> 
+            <i class="fa fa-window-minimize" aria-hidden="true"></i>
+          </span>
+
           Qty: 
-            <span class="cart-item__qty">${currentOrder[itemName]['qty']}</span>
+            <span class="cart-item__qty ${itemName}">${currentOrder[itemName]['qty']}</span>
         </div>
       </li>`;
-      let $cartItemQty = $('.cart-item__qty');
-      $cartItems.append(cartItem);
 
+      $cartItems.append(cartItem);
     } else {
       currentOrder[itemName]['qty'] += 1;
-      // $cartItemQty.remove();
-      // $cartItemQty.appendTo($('.cart-qty'));
+
+      let someNum = $('.cart-qty').find(`.${itemName}`);
+      someNum.remove();
+      let update = `<span class="cart-item__qty ${itemName}">${currentOrder[itemName]['qty']}</span>`;
+      // debugger;
+      
+      $('.cart-qty.' + itemName).append(update);   
     }
     console.log(currentOrder);
   });
+
+  // remove item from cart
+  $('.remove-item').on('click', function(e) {
+    
+  })
+
+  // reduce item qty from cart
+
+
+
 });
